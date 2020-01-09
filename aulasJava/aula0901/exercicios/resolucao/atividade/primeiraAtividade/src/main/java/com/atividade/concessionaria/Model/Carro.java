45dfb7c532ddf9a513cb875d14ad6aca079f27d6
Turma Generation <suellen.azevedo13@gmail.com>
2020-01-09 13:08:51 -0300

    Fim da aula. Não consegui concluir no tempo proposto (1 hora).

---
+++ aulasJava/aula0901/exercicios/resolucao/atividade/primeiraAtividade/src/main/java/com/atividade/concessionaria/Model/Carro.java
@@ -0,0 +1,10 @@
+package com.atividade.concessionaria.Model;
+
+public class Carro extends Produto{
+
+	public Carro(String nome, double preco) {
+		super(nome, preco);
+		// TODO Auto-generated constructor stub
+	}
+
+}
